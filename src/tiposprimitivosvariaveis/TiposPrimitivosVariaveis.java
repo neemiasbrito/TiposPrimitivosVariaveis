e53bf4a0a423769f102f4e6786b29d4f6e63525e
Neemais Brito <neeemiasaimeeen@gmail.com>
2021-12-05 22:43:58 -0300

    inicial

---
+++ src/tiposprimitivosvariaveis/TiposPrimitivosVariaveis.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposprimitivosvariaveis;
+
+/**
+ *
+ * @author HNROr
+ */
+public class TiposPrimitivosVariaveis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int idade = 10;
+        double preco = 10.5;
+        char sexo = 'M';
+        boolean temFilhos = true;
+        boolean temIrmão = false;
+        System.out.println("idade é: " + idade);
+        System.out.println("preço é: " + preco);
+        System.out.println("sexo é: " + sexo);
+        System.out.println("tem filhos: " + temFilhos);
+        System.out.println("tem irmão: " + temIrmão);
+        
+        
+    }
+    
+}
